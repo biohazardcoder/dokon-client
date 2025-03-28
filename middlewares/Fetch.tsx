04363a7d58d39f3d4313be7c065d602c6e3d2c9d
Husanbek <buxorojahon@gmail.com>
2025-03-28 16:41:54 +0500

    -v 1.1.0

--- middlewares/Fetch.tsx
+++ middlewares/Fetch.tsx
@@ -7,8 +7,7 @@ if (typeof window !== "undefined") {
 }
 
 export const Fetch = axios.create({
-  baseURL: "http://localhost:4000/",
-  // baseURL: "https://srv.s-market.uz/",
+  baseURL: "https://srv.s-market.uz/",
   headers: {
     Authorization: token,
   },
